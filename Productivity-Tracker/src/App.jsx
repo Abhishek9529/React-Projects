6951a2eb267d68d500afc486911958d659e2df06
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-07-25 14:20:41 +0530

    Update:  Productivity-Tracker

--- Productivity-Tracker/src/App.jsx
+++ Productivity-Tracker/src/App.jsx
@@ -1,62 +1,89 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { nanoid } from 'nanoid';
 import Test from "./components/Test";
 function App() {
 
 
   const [tasks, setTasks] = useState([])
-  const [currentInputTask, setCurrentInputTask] = useState('')
+  const [currentInputTask, setCurrentTask] = useState('')
+  const [isRunning, setIsRunning] = useState(false)
   const [seconds, setSeconds] = useState(0)
   const IntervalRef = useRef(null)
 
+  // load tasks from localeStorage
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem('tasks')) || []
+    setTasks(savedTasks)
+  }, [])
+
+
+  // save tasks to localestorage
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks))
   }, [tasks])
 
-  useEffect(()=>{
-  const allTasks = JSON.parse(localStorage.getItem('tasks')) || []
-  setTasks(allTasks)
-  },[])
+  const handleAddTask = async () => {
+    if (currentInputTask.trim() !== '') {
+      const allTasks = await JSON.parse(localStorage.getItem('tasks')) || []
+      allTasks.push({ name: currentInputTask, _id: nanoid(), startTime: seconds, completedTime: 0, totalTime: 0, isCompleted: false })
+      setTasks(allTasks)
+      handleStart()
+      setCurrentTask('')
+    }
+    // setTasks(prev => [...prev, { name: currentInputTask, _id: nanoid(), startTime: seconds, completedTime: 0, totalTime: 0, isCompleted: false }])
 
-  //  const allTasks = JSON.parse(localStorage.getItem('tasks')) || []
-  // console.log(allTasks);
-
-
-  const handleAddInput = () => {
-    if (currentInputTask != '') setTasks(prev => [...prev, { name: currentInputTask, tiem: seconds }])
-
-
-    setCurrentInputTask('')
   }
 
-  const handleStart = () => {
-    IntervalRef.current = setInterval(() => {
-      setSeconds(prev => prev + 1)
-      console.log(seconds);
-    }, 1000);
-  }
+  //Time Effect
+  useEffect(() => {
+    if (isRunning) {
+      IntervalRef.current = setInterval(() => {
+        setSeconds(prev => prev + 1)
+      }, 1000);
+    }
+    return () => {
+      clearInterval(IntervalRef.current)
+    }
+  }, [isRunning])
 
-  const handleStop = () => {
-    clearInterval(IntervalRef.current)
-  }
+  const handleStart = useCallback(() => {
+    setIsRunning(true)
+  }, [])
 
-  const handleReset = () => {
+  const handleStop = useCallback(() => {
+    setIsRunning(false)
+  }, [])
+
+  const handleReset = useCallback(() => {
     clearInterval(IntervalRef.current)
     setSeconds(0)
+  }, [])
+
+
+  const handleTaskCompleted = (id) => {
+    const updatedTasks = tasks.map(task => {
+      if (task._id === id && task.isCompleted === false) {
+        console.log("task Completed");
+        return { ...task, isCompleted: true, completedTime: seconds, totalTime: (seconds - task.startTime) }
+      }
+      return task
+    })
+    setTasks(updatedTasks)
   }
 
   return (
     <>
-      <div className="w-full h-screen flex flex-col gap-4 pt-5 items-center bg-gray-800 text-white">
+      <div className="w-full h-screen  flex flex-col gap-4 pt-5 items-center bg-gray-800 text-white">
         <h1>🧠 Productivity Tracker</h1>
         <div className="space-x-4">
           <input type="text"
             className="outline-none border rounded-md px-2 py-1"
             value={currentInputTask}
-            onChange={e => setCurrentInputTask(e.target.value)}
+            onChange={e => setCurrentTask(e.target.value)}
             placeholder="Enter task name"
           />
 
-          <button onClick={handleAddInput} className="px-2 py-1 rounded-md bg-blue-500">Add Task</button>
+          <button onClick={handleAddTask} className="px-2 py-1 rounded-md bg-blue-500">Add Task</button>
         </div>
 
         <div>
@@ -69,13 +96,24 @@ function App() {
         </div>
 
         <hr className="mt-4 w-full h-[1px] bg-white"></hr>
-        <div>
+        <div className=" w-[300px] overflow-y-scroll">
           <h1 className="mt-4 mb-4">📋 Task List:</h1>
           <ul>
             {
-              
-                tasks.map(task => (
-                <li className=" mb-2">Task: {task.name} Time: {task.time}</li>
+
+              tasks.map(task => (
+                <div className="flex flex-row justify-between items-center border rounded-md p-4 m-4 " key={Math.random()}>
+                  <div>
+                  <li className=" mb-2">Task: {task.name}</li>
+                  <h1  className="text-sm text-slate-400 ">Start Time: {task.startTime}</h1>
+                  <h1 className="text-sm text-slate-400 ">Completed Time: {task.isCompleted ? task.completedTime : 'Running' }</h1>
+                  <h1 className="text-sm text-slate-400 ">Status: {task.isCompleted ? 'Completed' : 'Running' }</h1>
+                  </div>
+                 
+                  <button className={` text-sm font-bold px-2 rounded-md ${task.isCompleted ? 'text-white bg-green-500' : 'text-black bg-white'}`} onClick={() => handleTaskCompleted(task._id)}>
+                    {task.isCompleted ? 'Completed' : 'Complete'}
+                  </button>
+                </div>
               ))
             }
           </ul>
