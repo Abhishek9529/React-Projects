fc3d9d5290ed8915e15e09bba1cad1785252c1a8
Abhishek9529 <abhishekchavhan61047@gmail.com>
2025-08-10 14:39:28 +0530

    Update ContactUs

--- ContactUs/src/App.jsx
+++ ContactUs/src/App.jsx
@@ -1,36 +1,50 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 function App() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
   const [errors, setErrors] = useState({})
-  const [isSubmitted, setIsSubmitted] = useState(false)
+  const nameRef = useRef(null)
+  const emailRef = useRef(null)
+  const messageRef = useRef(null)
 
-  console.log(errors);
+  const handleSubmit = (e) => {
+     e.preventDefault();
+     if(validateForm()){
+      console.log('Form Submitted:', {name, email, message});
+      setName('');
+      setEmail('');
+      setMessage('');
+      setErrors('');
+     }
+  };
 
+  const validateForm = () => {
 
-  const submitRequest = () => {
-    // if(!name.trim() || !email.trim() || !message.trim()) return alert('Enter all feild')
     let newErrors = {};
     if (!name.trim()) newErrors.name = 'Name is required';
-
     if (!email.trim()) {
       newErrors.email = 'Email is required';
     } else if (!/^\S+@\S+\.\S+$/.test(email)) {
       newErrors.email = 'Invalid email address';
     }
-
     if (!message.trim()) newErrors.message = 'Message is required';
     setErrors(newErrors)
 
-
-    // setIsSubmitted(true)
-    setName('')
-    setEmail('')
-    setMessage('')
-  }
-
+    // Focus first invalid field 
+    if (Object.keys(newErrors).length > 0) {
+      if (newErrors.name) {
+        nameRef.current.focus()
+      } else if (newErrors.email) {
+        emailRef.current.focus()
+      } else if (newErrors.message) {
+        messageRef.current.focus()
+      }
+      return false; // stop submission
+    }
+    return true; // form is valid 
+  };
 
   return (
     <>
@@ -39,9 +53,10 @@ function App() {
           <h2 className="text-white font-semibold text-lg mb-3 text-center">Contact Us</h2>
           <hr className="border-[#6b7280] mb-4" />
 
-          <div className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit}>
             <div>
               <input
+                ref={nameRef}
                 value={name}
                 onChange={e => setName(e.target.value)}
                 type="text"
@@ -55,6 +70,7 @@ function App() {
 
             <div>
               <input
+                ref={emailRef}
                 value={email}
                 onChange={e => setEmail(e.target.value)}
                 type="email"
@@ -68,6 +84,7 @@ function App() {
 
             <div>
               <textarea
+                ref={messageRef}
                 value={message}
                 onChange={e => setMessage(e.target.value)}
                 rows="6"
@@ -78,15 +95,13 @@ function App() {
                 {errors.message ? errors.message : ''}
               </span>
             </div>
-
             <button
-              onClick={() => submitRequest()}
               type="submit"
               className="border border-white text-[#6b7280] font-semibold text-base px-4 py-1"
             >
               Submit
             </button>
-          </div>
+          </form>
         </div>
       </div>
     </>
